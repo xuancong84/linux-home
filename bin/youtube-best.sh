e930f78d960eeeb3c45c219eb3c64a633108de6e
Wang Xuancong <xuancong@Wangs-MacBook-Pro.local>
2019-12-23 15:17:29 +0800

    added youtube-dl.sh and youtube-best.sh

---
+++ bin/youtube-best.sh
@@ -0,0 +1,3 @@
+
+youtube-dl -f bestvideo+bestaudio --merge-output-format mp4 $1
+
---
+++ bin/youtube-dl.sh
@@ -0,0 +1,4 @@
+
+youtube-dl -f bestvideo -o ~/beiwe-backend/files/yt1.mp4 --merge-output-format mp4 $1
+mv ~/beiwe-backend/files/yt1.mp4 ~/beiwe-backend/files/yt.mp4
+
