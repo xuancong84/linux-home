c1fc9f3ff91b2a5fd3c34242599f33cb2faccf28
xuancong84 <xuancong84@gmail.com>
2020-05-19 14:18:34 +0800

    for template.py, added ArgumentDefaultsHelpFormatter

--- bin/template2.py
+++ bin/template2.py
@@ -6,7 +6,8 @@ from NLP import *
 
 
 if __name__=='__main__':
-	parser = argparse.ArgumentParser(usage='$0 arg1 1>output 2>progress', description='what this program does')
+	parser = argparse.ArgumentParser(usage='$0 arg1 1>output 2>progress', description='what this program does',
+			formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 	parser.add_argument('positional', help='positional argument')
 	parser.add_argument('-optional', help='optional argument')
 	#nargs='?': optional positional argument; action='append': multiple instances of the arg; type=; default=
